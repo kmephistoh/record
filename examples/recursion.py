23f5f05f241eb2cde504c3c090ccf2fda0c8ac27
kevin <kanhaosmile@gmail.com>
2012-12-06 13:47:00 +0000

    修改编码

--- examples/recursion.py
+++ examples/recursion.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 import sys
 def factorial(n):
     if n==0:
@@ -7,5 +7,5 @@ def factorial(n):
     else:
         return n*factorial(n-1)
 
-print "今天一个简单的阶乘都写错了，罪过"
+print "今天一个简单的阶乘都写错了，罪过!"
 print sys.argv[1]+"的阶乘是：%d" % factorial(int(sys.argv[1])) 
