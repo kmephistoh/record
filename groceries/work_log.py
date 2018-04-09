24ef2748076d247bec51d65c8f47beea4f153c5b
kmephistoh <kanhaosmile@gmail.com>
2018-04-09 20:59:46 +0800

    Update work_log.py
    添加获取当前脚本的路径，在脚本所在的路径下生成日志目录

--- groceries/work_log.py
+++ groceries/work_log.py
@@ -7,6 +7,7 @@ Month = strftime('%Y-%m')
 Date = strftime('%Y-%m-%d')
 Time = strftime('%H:%M:%S')
 #top_dir = "D:work_log/"
+top_dir = os.path.dirname(os.path.abspath(__file__))
 dst_dir = Month   #if cwd is not top_dir: dst_dir = top_dir + month
 logfile = Date + '.txt'
 if not os.path.exists(dst_dir):
